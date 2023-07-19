3619d7fe46c80988dcc977da249588ac6b2087b8
OckiyaCliff <101569071+OckiyaCliff@users.noreply.github.com>
2023-07-19 12:38:54 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/blog/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Blog = () => {
+  return <div>This is the blog Page</div>;
+};
+
+export default Blog;
---
+++ src/app/talent/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TalentPage = () => {
+  return <div>This is the talent page</div>;
+};
+
+export default TalentPage;
